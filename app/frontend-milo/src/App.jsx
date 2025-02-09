dd7d3b4f1b25f1b9197e9c9112382a99a40acae2
Alem <alem.bicic60@myhunter.cuny.edu>
2025-02-08 22:30:10 -0500

    Added activity routing

--- app/frontend-milo/src/App.jsx
+++ app/frontend-milo/src/App.jsx
@@ -19,6 +19,7 @@ const App = () => {
       <Routes>
         <Route path="/loginpass" element={<PasswordPage />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/home/Activity" element={<Activity />} />
         <Route path="/" element={<UsernamePage/>} />
       </Routes>
     </Router>
